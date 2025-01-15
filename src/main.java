86447d88198a96c38e4d9a07993471f7967dd9f9
Maxence <maxence.timmerman@supinfo.com>
2025-01-15 14:28:19 +0100

    Main for connect database

--- src/main.java
+++ src/main.java
@@ -1,7 +1,27 @@
 package src;
 
-public class main {
-    public static void main(String[] args) {
+import repository.ConnexionRepository;
 
+import java.sql.Connection;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = ConnexionRepository.getConnection();
+        if (connection != null) {
+            System.out.println("Connexion établie avec succès !");
+            try {
+            } finally {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Erreur lors de la fermeture de la connexion.");
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("Échec de la connexion à la base de données.");
+        }
     }
 }
+
