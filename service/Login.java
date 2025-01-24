5a57166dc72147b919f4b17626be5584c30cc80e
hperonin <hugo.peronin@supinfo.com>
2025-01-25 00:15:01 +0100

    forget for after commit

--- service/Login.java
+++ service/Login.java
@@ -1,40 +1,45 @@
 package service;
 
-import repository.ConnexionRepository;
 import repository.UsersRepository;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Login {
-    public static void Login() {
+    public Login() {
+    }
+
+    public static void performLogin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(" --- Connexion --- ");
 
+        // Demande des informations d'authentification
         System.out.print("Entrez votre mail : ");
         String email = scanner.nextLine();
 
         System.out.print("Entrez votre mot de passe : ");
         String password = scanner.nextLine();
 
-        UsersRepository usersRepository = new UsersRepository();
-        if (usersRepository.authenticate(email, password)) {
-            System.out.println("La connexion à réussie. Vous pouvez maintenant naviguer");
-            String role = usersRepository.getRole(email);
-            if (role.equals("ADMIN")) {
-                AdminMenu adminMenu = new AdminMenu();
-                adminMenu.menu(email);
+        // Authentification de l'utilisateur
+        if (UsersRepository.authenticate(email, password)) {
+            System.out.println("La connexion a réussi. Vous pouvez maintenant naviguer.");
 
-            } else if (role.equals("USER")) {
+            // Récupération du rôle de l'utilisateur
+            String role = UsersRepository.getRole(email);
+
+            if ("ADMIN".equals(role)) {
+                AdminMenu adminMenu = new AdminMenu();
+                adminMenu.menu(email); // Navigation vers le menu admin
+            } else if ("USER".equals(role)) {
                 UserMenu userMenu = new UserMenu();
-                userMenu.menu(email);
+                userMenu.menu(email); // Navigation vers le menu utilisateur
+            } else {
+                System.out.println("Rôle inconnu. Contactez un administrateur.");
             }
 
         } else {
-            System.out.println("La connexion à échouée. Merci de vérifier vos identifiants.");
+            System.out.println("La connexion a échoué. Merci de vérifier vos identifiants.");
         }
-    }
 
+        // Fermeture du scanner
+        scanner.close();
+    }
 }
